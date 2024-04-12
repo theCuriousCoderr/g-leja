5bcdd9f8f8057c5d66af19b751cf1c78364575b2
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-04-12 08:22:28 +0100

    added a utils directory to handle dynamic content modification like the header links

---
+++ g-leja/src/utils/headerTabs.js
@@ -0,0 +1,10 @@
+const headerTabs = [
+    {keyword: "About", options: ["About", "G-Leja"]},
+    {keyword: "Product", options: ["Product", "G-Leja"]},
+    {keyword: "Customers", options: ["Customers", "G-Leja"]},
+    {keyword: "Prices", options: ["Prices", "G-Leja"]},
+    {keyword: "Our team", options: ["Our team", "G-Leja"]},
+    {keyword: "Blog", options: ["Blog", "G-Leja"]},
+]
+
+export default headerTabs;
