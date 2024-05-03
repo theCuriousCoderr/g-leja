dbf7d65cb2919e5562ecfb689433552719db8f90
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-05-03 15:48:44 +0100

    changed the gradient color of the how it works section

--- g-leja/src/pages/landing_page/LandingPage.jsx
+++ g-leja/src/pages/landing_page/LandingPage.jsx
@@ -175,14 +175,14 @@ function LandingPage() {
           <div className="px-16 flex flex-wrap justify-between gap-5 w-full">
             {howItWorks.map((reasons) => {
               return (
-                <button key={reasons.keyword} className="relative group w-[23%] aspect-square outline-orange-500 bg-gradient-to-r from-orange-500 to-orange-700 rounded-xl overflow-hidden">
+                <button key={reasons.keyword} className="relative group w-[23%] aspect-square outline-orange-500 bg-gradient-to-r from-orange-500 to-orange-600 rounded-xl overflow-hidden">
                   <div className="absolute size-full -right-32 -top-32 animate-ping flex items-center justify-center overflow-hidden">
                     <div className="absolute size-[5rem] rounded-full border border-orange-700"></div>
                     <div className="absolute size-[10rem] rounded-full border border-orange-700"></div>
                     <div className="absolute size-[15rem] rounded-full border border-orange-700"></div>
                     <div className="absolute size-[20rem] rounded-full border border-orange-700"></div>
                   </div>
-                  <div className="absolute z-10 bottom-4 left-4 group-hover:hidden group-focus-within:hidden transiton-all font-poppins text-white font-medium">
+                  <div className="absolute z-10 bottom-4 left-4 right-4 group-hover:hidden group-focus-within:hidden transiton-all font-poppins text-white font-medium text-left">
                     {reasons.keyword}
                   </div>
                   <div className="hidden size-full group-hover:flex group-focus-within:flex items-center justify-center text-sm transition-all font-inter p-3 text-white font-medium">
